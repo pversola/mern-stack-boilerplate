c03f10e76f413fe13bd9f3b42b7c6edc69ded137
Patrick Versola <patrickb.v@chanwanich.com>
2020-12-02 17:22:50 +0700

    test get data with axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import config from 'config'
 
 const App = () => {
+  const [data, setMessage] = useState({
+    message: ''
+  })
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+  const loadData = () => {
+    console.log(`${config.apiUrl}/api`)
+
+    axios({
+      method: 'GET',
+      url: `${config.apiUrl}/api`
+    })
+      .then((response) => {
+        console.log(`RESPONSE: ${response.data.message}`)
+        setMessage({
+          message: response.data.message
+        })
+      })
+      .catch((error) => {
+        console.log(`ERROR: ${response}`)
+      })
+  }
+
+  const { message } = data
   return (
     <div>
-      <h1>Hello world Test</h1>
+      <h1>{`${message}`}</h1>
     </div>
   )
 }
