c78ab7a974203576eb598ab88681537285e45c01
Patrick Versola <patrickb.v@chanwanich.com>
2020-12-09 10:45:17 +0700

    Add project expres-gateway

--- gateway/server.js
+++ gateway/server.js
@@ -1,30 +1,4 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const cors = require('cors');
-const schema = require('./schemas/schema');
+const path = require("path");
+const gateway = require("express-gateway");
 
-const app = express();
-
-const axios = require('axios');
-
-app.use(
-  cors({
-    origin: '*'
-  })
-);
-
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true
-  })
-);
-
-const port = process.env.PORT || '8080';
-
-app.listen(port, () => {
-  console.log(
-    `API gateway is running on port: ${port} (${process.env.NODE_ENV})`
-  );
-});
+gateway().load(path.join(__dirname, "config")).run();
